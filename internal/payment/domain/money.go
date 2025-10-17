ed6258ae519667f120d58059d2114889d1bc48fa
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-17 22:03:57 +0300

    adding domain logic w/ money.go

--- internal/payment/domain/money.go
+++ internal/payment/domain/money.go
@@ -11,6 +11,8 @@ type Money struct {
 	Scale    uint8  // Кол-во знаков после запятой
 }
 
+var scaleFactors = [9]int64{1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000}
+
 func NewMoney(amount int64, currency string, scale uint8) (Money, error) {
 	if scale > 8 {
 		return Money{}, errors.New("Scale is too large")
@@ -31,19 +33,11 @@ func (m Money) String() string {
 		amount = -amount
 	}
 
-	scaleFactor := pow10(int64(m.Scale))
+	scaleFactor := scaleFactors[m.Scale]
 
 	intPart := amount / scaleFactor
 	fractionPart := amount % 10
 
 	return fmt.Sprintf("%s%d.%0*d %s",
 		sign, intPart, m.Scale, fractionPart, m.Currency)
-}
-
-func pow10(n int64) int64 {
-	res := int64(1)
-	for i := int64(0); i < n; i++ {
-		res *= 10
-	}
-	return res
 }
