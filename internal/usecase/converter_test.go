f474d500ee6d5fac2b3c3bfa338f5bf5667abb94
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-18 23:55:46 +0300

    converter currency to rub test

---
+++ internal/usecase/converter_test.go
@@ -0,0 +1,53 @@
+package usecase
+
+import (
+	"payment-checker/internal/domain"
+	"testing"
+	"time"
+)
+
+const (
+	rateValue   = 10_0000 // Условно 10.0000 RUB за 1 USD
+	amountUSD   = 123_00  // 123.00 USD
+	fxCurrency  = "USD"
+	currencyRUB = "RUB"
+)
+
+func TestConverter_ToRUB(t *testing.T) {
+	date := time.Date(
+		2025,
+		3,
+		2,
+		0,
+		0,
+		0,
+		0,
+		time.UTC)
+
+	rate, err := domain.NewRate(date, fxCurrency, 1, rateValue) // 10.0000 RUB за USD
+	must(t, err)
+
+	conv := NewConverter(fakeFX{rate: rate})
+	money, err := domain.NewMoney(amountUSD, fxCurrency) // $123.00
+	must(t, err)
+
+	got, err := conv.ToRUB(money, date)
+	must(t, err)
+
+	expected := int64(amountUSD) * rateValue / domain.RateScale
+
+	if got.Currency != currencyRUB {
+		t.Fatalf("converted currency: %s (from %s), expected %s", got.Currency, fxCurrency, currencyRUB)
+	}
+
+	if got.Amount != expected {
+		t.Fatalf("got amount %d, expected %d", got.Amount, expected)
+	}
+}
+
+func must(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
