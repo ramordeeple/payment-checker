179f0d710181ce5a2e34ee691906964fd9d0f892
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-18 19:53:28 +0300

    adding centralized errors

---
+++ internal/domain/errors.go
@@ -0,0 +1,11 @@
+package domain
+
+import "errors"
+
+var (
+	ErrInvalidCurrencyCode = errors.New("invalid currency code")
+	ErrInvalidNominal      = errors.New("invalid rate nominal")
+	ErrInvalidRateValue    = errors.New("invalid rate value")
+	ErrRateNotFound        = errors.New("rate not found")
+	ErrDifferentCurrencies = errors.New("cannot provide an operation with different currencies")
+)
