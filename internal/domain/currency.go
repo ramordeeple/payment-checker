e1621823d1b926f4d69dd3f6a2fa4e2c7b3b8825
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-19 12:57:10 +0300

    deleting currency_codes.go due to its unnecessary and taking data from it to currency.go

--- internal/domain/currency.go
+++ internal/domain/currency.go
@@ -2,18 +2,9 @@ package domain
 
 type CurrencyCode string
 
-func validateCurrency(currency CurrencyCode) bool {
-
-	// Код валюты должен иметь строго 3 символа
-	if len(currency) != 3 {
-		return false
-	}
-
-	// Код валюты должен быть строго на латинице
-	for _, r := range currency {
-		if r < 'A' || r > 'Z' {
-			return false
-		}
-	}
-	return true
-}
+const (
+	CurrencyRUB = CurrencyCode("RUB")
+	CurrencyUSD = CurrencyCode("USD")
+	CurrencyEUR = CurrencyCode("EUR")
+	CurrencyJPY = CurrencyCode("JPY")
+)
--- internal/domain/currency_codes.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-// Выноска в отдельный файл, а не в currency.go, т.к. при большом кол-ве валют будет целеобразнее хранить их здесь
-const (
-	CurrencyRUB = CurrencyCode("RUB")
-	CurrencyUSD = CurrencyCode("USD")
-	CurrencyEUR = CurrencyCode("EUR")
-	CurrencyJPY = CurrencyCode("JPY")
-)
