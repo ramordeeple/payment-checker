e8e5e51e502d25f7a6f19b1fb2082b44284c6ef4
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-21 06:25:12 +0300

    removing unnecessary file and and repository

---
+++ internal/adapter/repository/rate_repo.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"database/sql"
+	"payment-checker/internal/domain"
+	"time"
+)
+
+type RateRepo struct {
+	db *sql.DB
+}
+
+func NewRateRepo(database *sql.DB) *RateRepo {
+	return &RateRepo{db: database}
+}
+
+func (r *RateRepo) GetRate(date time.Time, currency domain.CurrencyCode) (domain.Rate, error) {
+	var nominal int32
+	var valueScaled int64
+
+	row := r.db.QueryRow(`
+		SELECT nominal, value_scaled
+		FROM rates
+		WHERE date = $1 AND currency = $2`,
+		currency, date.Format("2006-01-02"))
+
+	if err := row.Scan(&nominal, &valueScaled); err != nil {
+		if err == sql.ErrNoRows {
+			return domain.Rate{}, domain.ErrRateNotFound
+		}
+		return domain.Rate{}, err
+	}
+
+	return domain.Rate{
+		Date:        date,
+		Currency:    currency,
+		Nominal:     nominal,
+		ValueScaled: valueScaled,
+	}, nil
+}
+
+func (r *RateRepo) HasCurrency(currency domain.CurrencyCode) bool {
+	var exists bool
+	row := r.db.QueryRow(`SELECT EXISTS (SELECT 1 FROM rates WHERE currency = $1)`, currency)
+
+	_ = row.Scan(&exists)
+
+	return exists
+}
