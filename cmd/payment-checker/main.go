a5b1b9c00603c12ece302c57a5b5dcba15ae7db9
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-10-21 06:36:34 +0300

    refactored main.go

--- cmd/payment-checker/main.go
+++ cmd/payment-checker/main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	_ "payment-checker/docs"
 	"payment-checker/internal/app"
 	"time"
-
-	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func main() {
@@ -21,40 +16,13 @@ func main() {
 	)
 	defer stop()
 
-	a := app.New()
+	a := app.InitDB(
+		"postgres",
+		"postgres://postgres:123@localhost:5433/payment_db?sslmode=disable")
 
-	http.Handle("/swagger/", httpSwagger.WrapHandler)
-
-	http.HandleFunc("/validate", a.Handler.ValidatePayment)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: http.DefaultServeMux,
-	}
-
-	go func() {
-		fmt.Printf("Listening http on port %s\n", server.Addr)
-		if err := server.ListenAndServe(); err != nil &&
-			err != http.ErrServerClosed {
-			log.Fatalf("Server closed with error: %s\n", err)
-		}
-	}()
-
-	go func() {
-		if err := a.StartGRPC(); err != nil {
-			log.Fatalf("grpc server closed with error: %s\n", err)
-		}
-	}()
-
+	httpSrv := a.StartHTTP(":8080")
+	grpcSrv := a.StartGRPC(":8090")
 	<-ctx.Done()
 
-	// Graceful shutdown
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := server.Shutdown(shutdownCtx); err != nil {
-		log.Fatalf("Server shutdown error: %s\n", err)
-	}
-
-	fmt.Println("Servers shutdown complete")
+	a.Shutdown(httpSrv, grpcSrv, 5*time.Second)
 }
