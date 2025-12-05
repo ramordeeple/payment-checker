14e4d40f9a3d97158abe32b4f1824ab0fcdded88
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-12-06 01:57:26 +0300

    Updated rate.go

--- internal/domain/rate.go
+++ internal/domain/rate.go
@@ -9,6 +9,9 @@ type Rate struct {
 	Currency    CurrencyCode
 	Nominal     int32
 	ValueScaled int64
+	CBRID       string
+	NumCode     string
+	NameRU      string
 }
 
 const RateScale = int64(10_000) // Чтобы можно было 75.1234 хранить как целое число(751234) во избежания ошибок округления
