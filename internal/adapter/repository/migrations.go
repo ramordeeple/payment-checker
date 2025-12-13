10cb3c546cbad165637bf97bf2ffdcd9d2cc39f9
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-12-13 22:24:45 +0300

    adding migrations.go

---
+++ internal/adapter/repository/migrations.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	_ "github.com/lib/pq"
+)
+
+func RunMigrations(db *sql.DB, migrationsDir string) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create migration driver: %w", err)
+	}
+
+	migrationsPath := fmt.Sprintf("file://%s", migrationsDir)
+
+	m, err := migrate.NewWithDatabaseInstance(
+		migrationsPath,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	return nil
+}
