90023f838dc42876b46553e358338fb9f3028cbd
scarabush_scarabush <snusikfrescus@gmail.com>
2026-08-28 05:57:01 +0300

    remove files

--- internal/domain/policy_reason.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type ValidationReason string
-
-const (
-	ReasonOK              ValidationReason = "OK"
-	ReasonRateUnavailable ValidationReason = "RATE_UNAVAILABLE"
-	ReasonLimitExceeded   ValidationReason = "LIMIT_EXCEEDED"
-)
