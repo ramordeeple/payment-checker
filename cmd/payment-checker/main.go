1733e18a6cf85ccb5a2a7da0f506df247602720e
scarabush_scarabush <snusikfrescus@gmail.com>
2026-08-28 05:59:58 +0300

    empty main

--- cmd/payment-checker/main.go
+++ cmd/payment-checker/main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"context"
-	"log"
-	"os"
-	"os/signal"
-	_ "payment-checker/docs"
-	"payment-checker/internal/app"
-	"time"
-)
-
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
 
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		log.Fatal("DATABASE_URL is not set")
-	}
-
-	a := app.InitDB("postgres", dsn, "file:///app/migrations")
-
-	httpSrv := a.StartHTTP(":8080")
-	grpcSrv, _, _ := a.StartGRPC(":8090")
-
-	<-ctx.Done()
-
-	a.Shutdown(httpSrv, grpcSrv, 5*time.Second)
 }
