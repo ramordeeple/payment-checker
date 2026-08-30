a6499549f477fffe5b82f0a5b434169b2fc5236e
scarabush_scarabush <snusikfrescus@gmail.com>
2026-08-31 00:10:56 +0300

    main running

--- cmd/payment-checker/main.go
+++ cmd/payment-checker/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
 
+	"payment-checker/internal/cbr"
+)
+
+func main() {
+	port := os.Getenv("REST_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle(
+		"GET /scripts/XML_daily.asp",
+		cbr.NewHandler(),
+	)
+
+	server := &http.Server{
+		Addr:              ":" + port,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       time.Minute,
+	}
+
+	log.Printf("CBR mock is listening on %s", server.Addr)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("HTTP server failed: %v", err)
+	}
 }
